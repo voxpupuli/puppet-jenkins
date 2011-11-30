36286ede08158519b2a4a5f31f94937c5f0bbd79
Jeff McCune <jeff@puppetlabs.com>
2011-11-30 10:07:31 -0800

    Add service resource to jenkins module
    Without this patch the jenkins module doesn't actually bring up the
    service.  This patch fixes the problem on EL based systems by adding a
    jenkins::service class.  The behavior for debian based systems remains
    the same, no service resource is managed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,24 @@
-
 class jenkins {
   include jenkins::repo
   include jenkins::package
+  include jenkins::service
 
-  Class["jenkins::repo"] -> Class["jenkins::package"]
+  Class["jenkins::repo"] -> Class["jenkins::package"] -> Class["jenkins::service"]
+}
+
+class jenkins::service {
+  case $::operatingsystem {
+    centos, redhat, oel: {
+      service { 'jenkins':
+        ensure     => running,
+        enable     => true,
+        hasstatus  => true,
+        hasrestart => true,
+      }
+    }
+    # Stay as a no-op to preserve previous behavior
+    default: { }
+  }
 }
 
 class jenkins::package {
