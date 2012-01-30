9d54458e0698c83875d6b9928571eb8eb8882390
Justin Ellison <justin.ellison@buckle.com>
2012-01-30 15:03:45 -0600

    Add support for auto-generating iptables ACCEPT rules for Jenkins
    This will only be enabled if you pull down the puppet-firewall module from
    Puppet Labs: <https://github.com/puppetlabs/puppetlabs-firewall>

---
+++ manifests/firewall.pp
@@ -0,0 +1,12 @@
+class jenkins::firewall {
+  if defined("firewall") {
+    firewall {
+      "500 allow Jenkins inbound traffic":
+        action => "accept",
+        state  => "NEW",
+        dport  => [8080],
+        proto  => "tcp",
+    }
+  }
+}
+
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class jenkins {
   include jenkins::repo
   include jenkins::package
   include jenkins::service
+  include jenkins::firewall
 
   Class["jenkins::repo"] -> Class["jenkins::package"] -> Class["jenkins::service"]
 
