71c37d420f45ce96b9f48c756a02209429209d80
R. Tyler Croy <tyler@monkeypox.org>
2012-12-18 22:01:27 -0800

    Workaround for lsbdistcodename with puppetlabs-apt module
    Fixes #30

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,8 +1,8 @@
 class jenkins::repo::debian {
   apt::source { 'jenkins':
     location    => 'http://pkg.jenkins-ci.org/debian',
-    release     => '',
-    repos       => 'binary/',
+    release     => 'binary/',
+    repos       => '',
     key         => 'D50582E6',
     key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
     include_src => false,
