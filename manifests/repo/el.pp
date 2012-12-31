55794c515d68669ee84a01541e6867d6cd0f88bb
Matthew Barr <mbarr@snap-interactive.com>
2012-12-31 13:04:19 -0500

    removed GPG key for RPM's, retrieve current GPG key via yumrepo provider.

--- manifests/repo/el.pp
+++ manifests/repo/el.pp
@@ -8,14 +8,7 @@ class jenkins::repo::el {
     descr    => 'Jenkins',
     baseurl  => 'http://pkg.jenkins-ci.org/redhat/',
     gpgcheck => 1,
-  }
-  file { '/etc/yum/jenkins-ci.org.key':
-    content => template("${module_name}/jenkins-ci.org.key"),
-  }
-  exec { 'rpm --import /etc/yum/jenkins-ci.org.key':
-    path    => '/bin:/usr/bin',
-    require => File['/etc/yum/jenkins-ci.org.key'],
-    unless  => 'rpm -q gpg-pubkey-d50582e6-4a3feef6',
+	gpgkey   => "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
   }
 }
 
