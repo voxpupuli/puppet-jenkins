7e1312914e71647a81c647698510b15d0f7d7dad
Igor Galić <i.galic@brainsware.org>
2013-03-25 21:39:10 +0100

    make puppet-lint happy
    This is step one in my master plan of "improving" this module.

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,24 +1,27 @@
 class jenkins::repo::debian {
-    if $lts == 0 {
-	    apt::source { 'jenkins':
-	      location    => 'http://pkg.jenkins-ci.org/debian',
-	      release     => 'binary/',
-	      repos       => '',
-	      key         => 'D50582E6',
-	      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-	      include_src => false,
-	    }
 
+  include 'jenkins::repo'
+
+  if $jenkins::repo::lts == 0 {
+    apt::source { 'jenkins':
+      location    => 'http://pkg.jenkins-ci.org/debian',
+      release     => 'binary/',
+      repos       => '',
+      key         => 'D50582E6',
+      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+      include_src => false,
     }
-    elsif $lts == 1 {
-	    apt::source { 'jenkins':
-	      location    => 'http://pkg.jenkins-ci.org/debian-stable',
-	      release     => 'binary/',
-	      repos       => '',
-	      key         => 'D50582E6',
-	      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-	      include_src => false,
-	    }
+
+  }
+  elsif $jenkins::repo::lts == 1 {
+    apt::source { 'jenkins':
+      location    => 'http://pkg.jenkins-ci.org/debian-stable',
+      release     => 'binary/',
+      repos       => '',
+      key         => 'D50582E6',
+      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+      include_src => false,
     }
-  
+  }
+
 }
--- manifests/repo/el.pp
+++ manifests/repo/el.pp
@@ -1,24 +1,24 @@
 class jenkins::repo::el ( $lts=0 )
 {
-  if $lts == 0 {
-	  yumrepo {'jenkins':
-	    descr    => 'Jenkins',
-	    baseurl  => 'http://pkg.jenkins-ci.org/redhat/',
-	    gpgcheck => 1,
-		gpgkey   => "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
-	  }
+
+  include 'jenkins::repo'
+
+  if $jenkins::repo::lts == 0 {
+    yumrepo {'jenkins':
+      descr    => 'Jenkins',
+      baseurl  => 'http://pkg.jenkins-ci.org/redhat/',
+      gpgcheck => 1,
+      gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+    }
   }
-  elsif $lts == 1 {
-	  yumrepo {'jenkins':
-	    descr    => 'Jenkins',
-	    baseurl  => 'http://pkg.jenkins-ci.org/redhat-stable/',
-	    gpgcheck => 1,
-		gpgkey   => "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
-	  }
+  elsif $jenkins::repo::lts == 1 {
+    yumrepo {'jenkins':
+      descr    => 'Jenkins',
+      baseurl  => 'http://pkg.jenkins-ci.org/redhat-stable/',
+      gpgcheck => 1,
+      gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+    }
   }
-  
-  
+
 }
-
-
 
