1756b9e00dd2e6d0b30ecf570f86e0a92fb6c1ac
Lars Fronius <l.fronius@googlemail.com>
2013-05-08 17:02:55 +0200

    fixup testing of module

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'puppet-module'
 group :development do
   gem 'cucumber'
   gem 'puppet-lint'
+  gem 'puppetlabs_spec_helper'
   gem 'rspec'
   gem 'rspec-puppet'
   gem 'blimpy', '~> 0.3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
 require 'puppet'
 require 'rubygems'
 require 'rspec-puppet'
-
-RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(File.dirname(__FILE__), '../../'))
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
