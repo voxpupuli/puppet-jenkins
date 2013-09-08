27ffe630681c8acfaddace76fc47cd4f327dfd5b
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-09-08 11:19:34 +0200

    Optionally manage plugin configuration with puppet
    The way this code is intended to be used is when managing a plugin and plugin configuration for a production Jenkins instance, using the files generated in a web UI of a staging instance.
    
    For example, if you have a test/staging instance of Jenkins where you validate
    your plugin configuration, you could take the myplugin.xml conifguration file
    from that machine, add it to your Git repository and reference it here.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,13 +1,22 @@
 #
+# config_filename = undef
+#   Name of the config file for this plugin.
 #
+# config_content = undef
+#   Content of the config file for this plugin. It is up to the caller to
+#   create this content from a template or any other mean.
 #
 define jenkins::plugin(
   $version=0
+  $manage_config   = false,
+  $config_filename = undef,
+  $config_content  = undef,
 ) {
 
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
   $plugin_parent_dir = inline_template('<%= @plugin_dir.split(\'/\')[0..-2].join(\'/\') %>')
+  validate_bool ($manage_config)
 
   if ($version != 0) {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
@@ -75,4 +84,18 @@ define jenkins::plugin(
     }
   }
 
+  if $manage_config {
+    if $config_filename == undef or $config_content == undef {
+      fail 'To deploy config file for plugin, you need to specify both $config_filename and $config_content'
+    }
+
+    file {"${plugin_parent_dir}/${config_filename}":
+      ensure  => present,
+      content => $config_content,
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      mode    => '0644',
+      notify  => Service['jenkins']
+    }
+  }
 }
