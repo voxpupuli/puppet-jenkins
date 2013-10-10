700eae208ed52f0b5d2c33725eb117b7d371fb37
Jeremy Christian <jchristi@redhat.com>
2013-10-10 11:12:12 -0400

    Add script that checks installed plugin version

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -5,6 +5,7 @@ define jenkins::plugin($version=0) {
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
   $plugin_parent_dir = '/var/lib/jenkins'
+  $plugin_installed  = '/tmp/plugin-is-installed'
 
   if ($version != 0) {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
@@ -44,13 +45,21 @@ define jenkins::plugin($version=0) {
     }
   }
 
+  if (!defined(File[$plugin_installed])) {
+    file {
+      $plugin_installed :
+        mode    => '0777',
+        content => template("jenkins/plugin-is-installed.erb");
+    }
+  }
+
   exec {
     "download-${name}" :
       command    => "wget --no-check-certificate ${base_url}${plugin}",
       cwd        => $plugin_dir,
-      require    => [File[$plugin_dir], Package['wget']],
+      require    => [File[$plugin_dir], Package['wget'], File[$plugin_installed]],
       path       => ['/usr/bin', '/usr/sbin',],
-      unless     => "test -f ${plugin_dir}/${name}.hpi || test -f ${plugin_dir}/${name}.jpi",
+      unless     => "bash ${plugin_installed} ${name} ${version}";
   }
 
   file {
@@ -58,6 +67,6 @@ define jenkins::plugin($version=0) {
       require => Exec["download-${name}"],
       owner   => 'jenkins',
       mode    => '0644',
-      notify  => Service['jenkins']
+      notify  => Service['jenkins'];
   }
 }
