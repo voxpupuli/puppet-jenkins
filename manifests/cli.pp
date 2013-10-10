0437639118673ebcda44ef19b803aba7df67d7bb
Jeremy Christian <jchristi@redhat.com>
2013-10-10 10:04:30 -0400

    Fix lint errors and spec test for jenkins::cli class

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -5,14 +5,14 @@
 class jenkins::cli {
 
   $jar = '/usr/lib/jenkins/jenkins-cli.jar'
-  $extract_jar = 'jar -xf /usr/lib/jenkins/jenkins.war WEB-INF/jenkins-cli.jar'
-  $move_jar = "mv WEB-INF/jenkins-cli.jar $jar"
+  $extract_jar = 'unzip /usr/lib/jenkins/jenkins.war WEB-INF/jenkins-cli.jar'
+  $move_jar = "mv WEB-INF/jenkins-cli.jar ${jar}"
   $remove_dir = 'rm -rf WEB-INF'
 
   exec { 'jenkins-cli':
-    command => "$extract_jar && $move_jar && $remove_dir",
+    command => "${extract_jar} && ${move_jar} && ${remove_dir}",
     path    => ['/bin', '/usr/bin'],
-    cwd     => '/root',
+    cwd     => '/tmp',
     creates => $jar,
     require => Package['jenkins'],
   }
