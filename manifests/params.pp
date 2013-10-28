7eb07a95eaa1f9b61ec86b1f5ef0256e4b7e96a5
Justin Lambert <jlambert@eml.cc>
2013-10-27 18:49:48 -0600

    allow disabling startup service or running service

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,11 @@ class jenkins::params {
   $version            = 'installed'
   $lts                = false
   $repo               = true
+  $service_enable     = true
+  $service_ensure     = 'running'
   $configure_firewall = true
   $install_java       = true
   $swarm_version      = '1.9'
 }
+
+
