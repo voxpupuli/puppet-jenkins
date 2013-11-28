5b7b9efa63db69ee213c16206804b546fd3eddb1
Jeremy Christian <jchristi@redhat.com>
2013-11-28 02:15:54 -0500

    Use new facter-based method of detecting installed plugin versions based on approach suggested by jlambert121

---
+++ lib/facter/jenkins.rb
@@ -0,0 +1,32 @@
+# jenkins.rb
+#
+# Creates a fact 'jenkins_plugins' containing a comma-delimited string of all
+# jenkins plugins + versions.
+#
+jenkins_home = Facter::Util::Resolution.exec("echo ~jenkins")
+plugins = "#{jenkins_home}/plugins"
+jenkins_plugins = ''
+
+if File.directory?(jenkins_home)
+  # Get a list of all plugins + versions
+  Dir.entries(plugins).select do |plugin|
+    if (File.directory?("#{plugins}/#{plugin}") == true) && !(plugin == '..' || plugin == '.')
+      begin
+	    contents = File.read("#{plugins}/#{plugin}/META-INF/MANIFEST.MF")
+      contents =~ (/Plugin\-Version:\s+([\d\.]+)/)
+      version = $1
+	    jenkins_plugins = "#{plugin} #{version}, " + jenkins_plugins
+      rescue
+        # Nothing really to do about it, failing means no version which will 
+        # result in a new plugin if needed
+      end
+    end
+  end
+  Facter.add('jenkins_plugins') do
+    setcode do
+      jenkins_plugins
+    end
+  end
+else
+  # Does not have jenkins installed
+end
--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -9,9 +9,11 @@ define jenkins::plugin($version=0) {
 
   if ($version != 0) {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
+    $search   = "${name} ${version},"
   }
   else {
     $base_url = 'http://updates.jenkins-ci.org/latest/'
+    $search   = "${name} "
   }
 
   if (!defined(File[$plugin_dir])) {
@@ -47,21 +49,14 @@ define jenkins::plugin($version=0) {
     }
   }
 
-  if (!defined(File[$plugin_installed])) {
-    file {
-      $plugin_installed :
-        mode    => '0777',
-        content => template("jenkins/plugin-is-installed.erb");
+  if (empty(grep([ $::jenkins_plugins ], $search))) {
+    exec {
+      "download-${name}" :
+        command    => "rm -rf ${name} ${name}.* && wget --no-check-certificate ${base_url}${plugin}",
+        cwd        => $plugin_dir,
+        require    => [File[$plugin_dir], Package['wget']],
+        path       => ['/usr/bin', '/usr/sbin',],
     }
-  }
-
-  exec {
-    "download-${name}" :
-      command    => "rm -rf ${name} ${name}.* && wget --no-check-certificate ${base_url}${plugin}",
-      cwd        => $plugin_dir,
-      require    => [File[$plugin_dir], Package['wget'], File[$plugin_installed]],
-      path       => ['/usr/bin', '/usr/sbin',],
-      unless     => "bash ${plugin_installed} ${name} ${version}";
   }
 
   file {
@@ -71,4 +66,5 @@ define jenkins::plugin($version=0) {
       mode    => '0644',
       notify  => Service['jenkins'];
   }
+
 }
