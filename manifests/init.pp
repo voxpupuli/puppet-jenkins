26bdc384b38e8d5d17133a9a2dfc01cc969e7663
Jeremy Christian <jchristi@redhat.com>
2013-12-09 11:31:57 -0500

    Revert old commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -78,7 +78,7 @@ class jenkins(
   $install_java       = $jenkins::params::install_java,
   $proxy_host         = undef,
   $proxy_port         = undef,
-  $cli				  = undef,
+  $cli				        = undef,
 ) inherits jenkins::params {
 
   $lts_real = str2bool($lts)
