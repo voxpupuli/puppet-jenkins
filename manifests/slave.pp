b885b98ccd01c8376d05c6bf0dd94c303c409745
Nick Meier <nmeier@microsoft.com>
2014-01-09 18:02:45 -0500

    Converted exec that started the service to a service resource.  Added subscribe to service resource to it will be restarted if the .xml file changes

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -205,10 +205,17 @@ class jenkins::slave (
         unless  => "${::systemdrive}\\windows\\system32\\sc.exe getdisplayname JenkinsSlave",
       }
 
-      exec { 'sc_start_jenkinsslave':
-        command => "${::systemdrive}\\windows\\system32\\sc.exe start JenkinsSlave",
-        returns => [0, 1056],
-        require => Exec[ 'sc_create_service' ],
+      #exec { 'sc_start_jenkinsslave':
+      #  command => "${::systemdrive}\\windows\\system32\\sc.exe start JenkinsSlave",
+      #  returns => [0, 1056],
+      #  require => Exec[ 'sc_create_service' ],
+      #}
+
+      service { 'jenkinsslave':
+        ensure  => running,
+        enable  => true,
+        require => Exec[sc_create_service],
+        subscribe => File[ "${slave_home}\\jenkinsslave.xml" ],
       }
     }
     default: {
