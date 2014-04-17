b30e9e4ee0e10ad579a515bc470cdb00bfcf32f5
matt urbanski <matt@iflowfor8hours.info>
2014-04-17 11:41:41 -0500

    Add support for debian slaves to disable ssl verification

--- spec/classes/jenkins_slave_spec.rb
+++ spec/classes/jenkins_slave_spec.rb
@@ -24,8 +24,17 @@ describe 'jenkins::slave' do
   end
 
   describe 'Debian' do
-    let(:facts) { { :ostype => 'Debian' } }
-    it { expect { should raise_error(Puppet::Error) } }
+    let(:facts) { { :osfamily => 'Debian', :lsbdistid => 'debian', :lsbdistcodename => 'natty', :operatingsystem => 'Debian' } }
+    describe 'default' do
+      it { should contain_exec('get_swarm_client') }
+      it { should contain_file('/etc/init.d/jenkins-slave') }
+      it { should contain_service('jenkins-slave') }
+      it { should contain_user('jenkins-slave_user').with_uid(nil) }
+    end
+    describe 'with ssl verification disabled' do
+      let(:params) { { :disable_ssl_verification => true } }
+      it { should contain_file('/etc/default/jenkins-slave').with_content(/-disableSslVerification/) }
+    end
   end
 
   describe 'Unknown' do
