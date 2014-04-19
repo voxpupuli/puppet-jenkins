1437e2e089c95af67d470ecdfbbc6153aa024d39
R. Tyler Croy <tyler@monkeypox.org>
2014-04-19 12:24:52 -0700

    Require the jpm ruby gem to be installed in order for our custom provider to work
    jpm is located: <https://github.com/rtyler/jpm>
    
    References #10, #11, #12

--- lib/puppet/features/json.rb
+++
@@ -1,5 +0,0 @@
-# Add the 'json' feature to help confine our custom providers
-#
-# This comes from: <http://alcy.github.io/2012/11/21/handling-gem-dependencies-in-custom-puppet-providers/>
-
-Puppet.features.add(:json, :libs => ["json"])
--- spec/unit/jenkins_provider_spec.rb
+++ spec/unit/jenkins_provider_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-require 'puppet/provider/package/jenkins'
+require 'puppet/provider/package/jpm'
 
-provider_type = Puppet::Type.type(:package).provider(:jenkins)
+provider_type = Puppet::Type.type(:package).provider(:jpm)
 
 describe provider_type do
 end
