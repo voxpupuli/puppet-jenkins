292431cdb838a27768b1a5ca082e0866a41b3cf4
Bruno Bieth <bruno.bieth@nespresso.com>
2014-05-02 14:04:54 +0200

    Parameterize jenkins plugin repository and add support for core plugins
    This is useful when you want jenkins to download plugins from an enterprise
    repository.
    
    As for the core plugin support this should fix
    https://github.com/jenkinsci/puppet-jenkins/issues/118

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,13 +2,17 @@
 #
 #
 class jenkins::params {
-  $version            = 'installed'
-  $lts                = false
-  $repo               = true
-  $service_enable     = true
-  $service_ensure     = 'running'
-  $install_java       = true
-  $swarm_version      = '1.9'
+  $version                       = 'installed'
+  $lts                           = false
+  $repo                          = true
+  $service_enable                = true
+  $service_ensure                = 'running'
+  $install_java                  = true
+  $swarm_version                 = '1.9'
+
+  $plugin_repository_base_url    = 'http://updates.jenkins-ci.org/download/plugins/'
+  $plugin_repository_maven_style = false
+  $default_plugin_group_id       = 'org.jenkins-ci.plugins'
 }
 
 
