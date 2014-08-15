09dd71f455c8577e72929a390f3956ae9fb710a5
David Snyder <david@infochimps.com>
2014-08-15 17:51:35 -0500

    Ensure plugin download exec is idempotent

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -74,6 +74,7 @@ define jenkins::plugin(
       cwd        => $plugin_dir,
       require    => [File[$plugin_dir], Package['wget']],
       path       => ['/usr/bin', '/usr/sbin', '/bin'],
+      unless     => "test -f ${plugin}",
     }
 
     file { "${plugin_dir}/${plugin}" :
