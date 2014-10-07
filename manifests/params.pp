ee5a5e5504a24719784f7b08ddef98fdbadfd873
Hylke Stapersma <hylke.stapersma@gmail.com>
2014-10-07 11:34:19 +0200

    Upgraded swarm cli client 0.17 -> 1.19 and added support for autodiscovery parameter

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class jenkins::params {
   $service_enable             = true
   $service_ensure             = 'running'
   $install_java               = true
-  $swarm_version              = '1.17'
+  $swarm_version              = '1.19'
   $default_plugins_host       = 'http://updates.jenkins-ci.org'
 
   case $::osfamily {
