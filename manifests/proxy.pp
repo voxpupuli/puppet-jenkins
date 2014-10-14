449d6c1e949321ff368587860671f61564d3abf6
Thomas Linkin <trlinkin@gmail.com>
2014-10-14 12:20:30 -0400

    Improve how `::jenkins::proxy` accesses variables
    Without this commit, the template referenced by the class
    `::jenkins::proxy` was using the `lookupvar()` method in the ERB
    tags to access variables from other class scopes. This method of
    accessing the variables is at the mercy of changes to Puppet's internal
    Ruby API. This method is also not very clear as to what is happening
    without having to read through both the class and the template.
    
    This commit changes the method of variable access to use a simple
    variable assignment operation in the Puppet DSL to bring the values of
    the required variables into the local scope. This is preferred as it
    makes the template easier to read and keeps the template's operations
    relative to the place from where it is called. In the Puppet code
    itself, it is now more clear as to what data is used by the
    `::jenkins::proxy` class and where that data is coming from.

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -5,6 +5,9 @@ class jenkins::proxy {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  # Bring variables from Class['::jenkins'] into local scope.
+  $proxy_host = $::jenkins::proxy_host
+  $proxy_port = $::jenkins::proxy_port
   $no_proxy_list = $::jenkins::no_proxy_list
 
   file { '/var/lib/jenkins/proxy.xml':
