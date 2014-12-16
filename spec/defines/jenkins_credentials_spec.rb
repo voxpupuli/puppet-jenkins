48186002a981632a049533d03bb864be9d66c38c
Andrew Stangl <andrewstangl@gmail.com>
2014-12-16 17:21:17 +0200

    add spec tests for jenkins::credentials define
    massage travis into playing nice
    
    Revert "massage travis into playing nice"
    
    This reverts commit 86480e3c3a2fe19dd0aea67abd7ccc1ba3507959.

--- spec/defines/jenkins_credentials_spec.rb
+++ spec/defines/jenkins_credentials_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe 'jenkins::credentials', :type => :define do
   let(:title) { 'foo' }
   let(:facts) {{ :osfamily => 'RedHat', :operatingsystem => 'RedHat' }}
+  let(:helper_cmd) { "/usr/bin/java -jar cli.jar -s http://127.0.0.1:8080 groovy /var/lib/jenkins/puppet_helper.groovy" }
+  let(:pre_condition) {
+    "class jenkins::cli_helper { $helper_cmd = '#{helper_cmd}' }"
+  }
 
   describe 'relationships' do
     let(:params) {{ :password => 'foo' }}
@@ -15,4 +19,47 @@ describe 'jenkins::credentials', :type => :define do
         that_comes_before('Anchor[jenkins::end]')
     end
   end
+
+  describe 'with ensure is present' do
+    let(:params) {{
+      :ensure   => 'present',
+      :password => 'mypass',
+    }}
+    it { should contain_exec('create-jenkins-credentials-mycreds').with({
+      :command    => "#{helper_cmd} create_or_update_credentials #{title} 'mypass' '' 'Managed by Puppet' ''",
+      :unless     => "#{helper_cmd} credential_info #{title} | grep #{title}",
+      :tries      => '3',
+      :try_sleep  => '5',
+      :require    => 'Class[Jenkins::Cli_helper]',
+    })}
+  end
+
+  describe 'with ensure is absent' do
+    let(:params) {{
+      :ensure   => 'absent',
+      :password => 'mypass',
+    }}
+    it { should contain_exec('delete-jenkins-credentials-mycreds').with({
+      :command    => "#{helper_cmd} delete_credentials #{title}",
+      :tries      => '3',
+      :try_sleep  => '5',
+      :require    => 'Class[Jenkins::Cli_helper]',
+    })}
+  end
+
+  describe 'with uuid set' do
+    let(:params) {{
+      :ensure   => 'present',
+      :password => 'mypass',
+      :uuid     => 'e94d3b98-5ba4-43b9-89ed-79a08ea97f6f',
+    }}
+    it { should contain_exec('create-jenkins-credentials-mycreds').with({
+      :command    => "#{helper_cmd} create_or_update_credentials #{title} 'mypass' 'e94d3b98-5ba4-43b9-89ed-79a08ea97f6f' 'Managed by Puppet' ''",
+      :unless     => "#{helper_cmd} credential_info #{title} | grep #{title}",
+      :tries      => '3',
+      :try_sleep  => '5',
+      :require    => 'Class[Jenkins::Cli_helper]',
+    })}
+  end
+
 end
