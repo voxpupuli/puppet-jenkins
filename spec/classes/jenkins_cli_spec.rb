voxpupuli/puppet-jenkins

commit 979cb012c12bc80306a88465f8cd78f43b32f70d

move Exec['reload-jenkins'] from jenkins::cli -> jenkins::cli::reload
Due to resource containment, it was not possible for a consumer of
`jenkins::cli` to both have a dependency on `Class['jenkins::cli']` and to
notify `Exec['reload-jenkins']`.  It is recommended that notifications be
instead be sent to `Class['jenkins::cli::reload']` to ease any future
refactoring.

manifests/cli.pp
@@ -31,16 +31,6 @@ class jenkins::cli {
   # The jenkins cli command with required parameter(s)
   $cmd = "java -jar ${jar} -s http://localhost:${port}"
 
-  # Reload all Jenkins config from disk (only when notified)
-  exec { 'reload-jenkins':
-    command     => "${cmd} reload-configuration",
-    path        => ['/bin', '/usr/bin'],
-    tries       => 10,
-    try_sleep   => 2,
-    refreshonly => true,
-    require     => File[$jar],
-  }
-
   # Do a safe restart of Jenkins (only when notified)
   exec { 'safe-restart-jenkins':
     command     => "${cmd} safe-restart && /bin/sleep 10",

manifests/init.pp
@@ -199,6 +199,7 @@ class jenkins(
 
   if $cli {
     include jenkins::cli
+    include jenkins::cli::reload
   }
 
   Anchor['jenkins::begin'] ->
@@ -213,8 +214,9 @@ class jenkins(
     Anchor['jenkins::begin'] ->
       Class['jenkins::service'] ->
         Class['jenkins::cli'] ->
-          Class['jenkins::jobs'] ->
-            Anchor['jenkins::end']
+          Class['jenkins::cli::reload'] ->
+            Class['jenkins::jobs'] ->
+              Anchor['jenkins::end']
   }
 
   if $install_java {

manifests/cli/reload.pp
@@ -1,0 +1,19 @@
+# Class: jenkins::cli::reload
+#
+# Command Jenkins to reload config.xml via the CLI.
+#
+class jenkins::cli::reload {
+
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
+  # Reload all Jenkins config from disk (only when notified)
+  exec { 'reload-jenkins':
+    command     => "${::jenkins::cli::cmd} reload-configuration",
+    path        => ['/bin', '/usr/bin'],
+    tries       => 10,
+    try_sleep   => 2,
+    refreshonly => true,
+  }
+}

commit 2b2109f402b83f43d06b07798e5eb93085b4dedb

fix Exec['reload-jenkins'] relationship regression
Introduced by: https://github.com/jenkinsci/puppet-jenkins/commit/979cb012c12bc80306a88465f8cd78f43b32f70d

Classes/defines which include the jenkins::cli class assume that they can use
the cli even if $::jenkins::cli == false.  This breaks the top level anchor
pattern.  The cli param should either be deprecated and essentially hardwired
to true or attempting to use cli functionality without this param set should
fail; either option is a backwards incompatible change.

As an attempt to preserve backwards compatibility, there are includes and
resource relationships being scattered throughout this module.

manifests/cli.pp
@@ -3,10 +3,24 @@
 # Allow Jenkins commands to be issued from the command line
 #
 class jenkins::cli {
-
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
+
+  include ::jenkins
+
+  # XXX Classes/defines which include the jenkins::cli class assume that they
+  # can use the cli even if $::jenkins::cli == false.  This breaks the top
+  # level anchor pattern.  The cli param should either be deprecated and
+  # essentially hardwired to true or attempting to use cli functionality
+  # without this param set should fail; either option is a backwards
+  # incompatible change.
+  #
+  # As an attempt to preserve backwards compatibility, there are includes and
+  # resource relationships being scattered throughout this module.
+  Class['jenkins::service'] ->
+    Class['jenkins::cli'] ->
+      Anchor['jenkins::end']
 
   $jar = "${jenkins::libdir}/jenkins-cli.jar"
   $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/jenkins-cli.jar"
@@ -40,4 +54,8 @@ class jenkins::cli {
     refreshonly => true,
     require     => File[$jar],
   }
+
+  # jenkins::cli::reload should be included only after $::jenkins::cli::cmd is
+  # defined
+  include ::jenkins::cli::reload
 }

manifests/init.pp
@@ -226,7 +226,6 @@ class jenkins(
 
   if $cli {
     include jenkins::cli
-    include jenkins::cli::reload
   }
 
   if $executors {
@@ -247,15 +246,6 @@ class jenkins(
           Class['jenkins::service'] ->
             Class['jenkins::jobs'] ->
               Anchor['jenkins::end']
-
-  if $cli {
-    Anchor['jenkins::begin'] ->
-      Class['jenkins::service'] ->
-        Class['jenkins::cli'] ->
-          Class['jenkins::cli::reload'] ->
-            Class['jenkins::jobs'] ->
-              Anchor['jenkins::end']
-  }
 
   if $install_java {
     Anchor['jenkins::begin'] ->

spec/classes/jenkins_cli_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'jenkins', :type => :module do
+describe 'jenkins', :type => :class do
   let(:facts) { { :osfamily => 'RedHat', :operatingsystem => 'RedHat' } }
 
   context 'cli' do
@@ -12,12 +12,24 @@ describe 'jenkins', :type => :module do
       let(:params) {{ :cli => true,
                       :config_hash => { 'HTTP_PORT' => { 'value' => '9000' } }
       }}
-      it { should create_class('jenkins::cli') }
+      it { should contain_class('jenkins::cli') }
       it { should contain_exec('jenkins-cli') }
       it { should contain_exec('reload-jenkins').with_command(/http:\/\/localhost:9000/) }
       it { should contain_exec('safe-restart-jenkins') }
       it { should contain_jenkins__sysconfig('HTTP_PORT').with_value('9000') }
+
+      describe 'jenkins::cli' do
+        describe 'relationships' do
+          it do
+            should contain_class('jenkins::cli').
+              that_requires('Class[jenkins::service]')
+          end
+          it do
+            should contain_class('jenkins::cli').
+              that_comes_before('Anchor[jenkins::end]')
+          end
+        end
+      end
     end
   end
-
 end