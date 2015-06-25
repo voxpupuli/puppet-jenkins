56b977b502d64429bbadaa7041f1d7bda79d1b84
Toni Schmidbauer <toni@stderr.at>
2015-06-25 16:31:16 +0200

    yum_proxy was renamed to repo_proxy so fix the spec

--- spec/classes/jenkins_repo_el_spec.rb
+++ spec/classes/jenkins_repo_el_spec.rb
@@ -16,8 +16,8 @@ describe 'jenkins', :type => :module do
       it { should contain_yumrepo('jenkins').with_proxy(nil) }
     end
 
-    describe 'yumproxy is set' do
-      let(:params) { { :yum_proxy => 'http://proxy:8080/' }}
+    describe 'repo_proxy is set' do
+      let(:params) { { :repo_proxy => 'http://proxy:8080/' }}
       it { should contain_yumrepo('jenkins').with_proxy('http://proxy:8080/') }
     end
   end
