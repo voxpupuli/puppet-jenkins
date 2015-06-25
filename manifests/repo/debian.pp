34391bf898ac42b52449c007e18a789bcc744a6f
Henrik Nicolaisen <hmn@area-51.dk>
2015-06-25 12:18:31 +0200

    use the new puppetlabs-apt version 2.x module

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -14,9 +14,11 @@ class jenkins::repo::debian
       location    => 'http://pkg.jenkins-ci.org/debian-stable',
       release     => 'binary/',
       repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      key         => {
+        'id'      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        'source'  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
+      },
+      include     => { 'src' => false }
     }
   }
   else {
@@ -24,9 +26,11 @@ class jenkins::repo::debian
       location    => 'http://pkg.jenkins-ci.org/debian',
       release     => 'binary/',
       repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      key         => {
+        'id'      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        'source'  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
+      },
+      include     => { 'src' => false }
     }
   }
 
