6b55e4b3c9d5b3812d83db77cc8ebda8b3a6ce63
Nayeem Syed <developerinlondon@gmail.com>
2015-12-03 16:17:53 +0000

    fix deprecation warnings

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -11,22 +11,22 @@ class jenkins::repo::debian
 
   if $::jenkins::lts  {
     apt::source { 'jenkins':
-      location    => 'http://pkg.jenkins-ci.org/debian-stable',
-      release     => 'binary/',
-      repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      location => 'http://pkg.jenkins-ci.org/debian-stable',
+      release  => 'binary/',
+      repos    => '',
+      key      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+      source   => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+      include  => { 'src' => false },
     }
   }
   else {
     apt::source { 'jenkins':
-      location    => 'http://pkg.jenkins-ci.org/debian',
-      release     => 'binary/',
-      repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      location => 'http://pkg.jenkins-ci.org/debian',
+      release  => 'binary/',
+      repos    => '',
+      key      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+      source   => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+      include  => { 'src' => false },
     }
   }
 
