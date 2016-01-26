45bad5de6bd5178688a30f1a384622b6a805beda
Nicolas Glayre <nicolas.glayre@nespresso.com>
2016-01-26 10:44:38 +0100

    Correcting conflicts

--- manifests/job/present.pp
+++ manifests/job/present.pp
@@ -37,36 +37,33 @@ define jenkins::job::present(
   }
 
   $jenkins_cli        = $jenkins::cli::cmd
-  if $temp_config_path == undef {
-    if $config == undef {
-      fail('If you don\'t give a temp_config_path you need to set the config param')
-    }
-    $tmp_config_path    = "/tmp/${jobname}-config.xml"
-    #
-    # When a Jenkins job is imported via the cli, Jenkins will
-    # re-format the xml file based on its own internal rules.
-    # In order to make job management idempotent, we need to
-    # apply that formatting before the import, so we can do a diff
-    # on any pre-existing job to determine if an update is needed.
-    #
-    # Jenkins likes to change single quotes to double quotes
-    $a = regsubst($config, 'version=\'1.0\' encoding=\'UTF-8\'',
-                  'version="1.0" encoding="UTF-8"')
-    # Change empty tags into self-closing tags
-    $b = regsubst($a, '<([A-z]+)><\/\1>', '<\1/>', 'IG')
-    # Change &quot; to " since Jenkins is weird like that
-    $c = regsubst($b, '&quot;', '"', 'MG')
-    # Change &apos; to ' since Jenkins is weird like that
-    $d = regsubst($c, '&apos;', '\'', 'MG')
-
-    # Temp file to use as stdin for Jenkins CLI executable
-    file { $tmp_config_path:
-      content => $d,
-      require => Exec['jenkins-cli'],
-    }
-  } else {
+  if $config == undef {
     $tmp_config_path = $temp_config_path
-  }
+  } else {
+      $tmp_config_path    = "/tmp/${jobname}-config.xml"
+      #
+      # When a Jenkins job is imported via the cli, Jenkins will
+      # re-format the xml file based on its own internal rules.
+      # In order to make job management idempotent, we need to
+      # apply that formatting before the import, so we can do a diff
+      # on any pre-existing job to determine if an update is needed.
+      #
+      # Jenkins likes to change single quotes to double quotes
+      $a = regsubst($config, 'version=\'1.0\' encoding=\'UTF-8\'',
+                    'version="1.0" encoding="UTF-8"')
+      # Change empty tags into self-closing tags
+      $b = regsubst($a, '<([A-z]+)><\/\1>', '<\1/>', 'IG')
+      # Change &quot; to " since Jenkins is weird like that
+      $c = regsubst($b, '&quot;', '"', 'MG')
+      # Change &apos; to ' since Jenkins is weird like that
+      $d = regsubst($c, '&apos;', '\'', 'MG')
+
+      # Temp file to use as stdin for Jenkins CLI executable
+      file { $tmp_config_path:
+        content => $d,
+        require => Exec['jenkins-cli'],
+      }
+    }
 
   $job_dir            = "${::jenkins::job_dir}/${jobname}"
   $config_path        = "${job_dir}/config.xml"
