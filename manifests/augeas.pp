677d2db5a38492ed2fff2e7f478c88b93ff676b0
Jan Vansteenkiste <jan@vstone.eu>
2016-01-26 06:56:08 +0100

    Introduced a helper to deal with augeas and configuration (and plugins)

---
+++ manifests/augeas.pp
@@ -0,0 +1,74 @@
+# == Definition jenkins::augeas
+#
+# === Parameters:
+#
+# [*config_filename*]:  Filename of the configuration file to work on relative to the jenkins localstatedir.
+#
+# [*changes*]:          String or array with augeas changes to perform.
+#
+# [*onlyif*]:           Optional augeas command and comparisons to control the execution of this type.
+#
+# [*plugin*]:           Optionally jenkins::augeas can also install the plugin. If this is set to true,
+#                       we use the name of the resource as plugin name. If it's a string, that is used
+#                       as plugin name.
+#
+# [*plugin_version*]:   Optional plugin version to pass through to jenkins::plugin
+#
+# [*context*]:          Optional context to ease your change rules.
+#
+#
+# === Example:
+#
+#     jenkins::augeas {'git':
+#       plugin          => true,
+#       config_filename => 'hudson.plugins.git.GitSCM.xml',
+#       context         => '/hudson.plugins.git.GitSCM_-DescriptorImpl',
+#       changes         => [
+#         'set globalConfigName/#text "Bob the Builder"',
+#         'set globalConfigEmail/#text "bob@example.com",
+#       ],
+#     }
+#
+#
+#
+define jenkins::augeas (
+  $config_filename,
+  $changes,
+  $onlyif           = undef,
+  $context          = '/',
+  $plugin_version   = undef,
+  $plugin           = false,
+) {
+
+  include ::jenkins
+
+  case $plugin {
+    true,'true': { # lint:ignore:quoted_booleans
+      jenkins::plugin {$name:
+        version       => $plugin_version,
+        manage_config => false,
+        before        => Augeas["jenkins::augeas: ${name}"],
+      }
+    }
+    false,'false': { # lint:ignore:quoted_booleans
+      #do nothing
+    }
+    default: {
+      jenkins::plugin {$plugin:
+        version       => $plugin_version,
+        manage_config => false,
+        before        => Augeas["jenkins::augeas: ${name}"],
+      }
+    }
+  }
+
+  augeas {"jenkins::augeas: ${name}":
+    incl    => "${::jenkins::localstatedir}/${config_filename}",
+    lens    => 'Xml.lns',
+    context => regsubst("/files${::jenkins::localstatedir}/${config_filename}/${context}", '\/\/', '/'),
+    notify  => Exec['reload-jenkins'],
+    changes => $changes,
+  }
+
+
+}
---
+++ spec/defines/jenkins_augeas_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe 'jenkins::augeas' do
+
+  let(:title) { 'myplug' }
+  let(:facts) {{ :osfamily => 'RedHat', :operatingsystem => 'CentOS' }}
+
+
+  [false,'false'].each do |pval|
+    describe "with plugin param #{pval} (#{pval.class})" do
+      let (:params) {{ :config_filename => 'foo.xml', :changes => ['set foo bar'], :plugin => pval }}
+      it do
+        should contain_augeas('jenkins::augeas: myplug').with(
+          :incl    => "/var/lib/jenkins/foo.xml",
+          :context => "/files/var/lib/jenkins/foo.xml/",
+          :changes => ['set foo bar'],
+          :lens    => 'Xml.lns',
+        )
+        should_not contain_jenkins__plugin('myplug')
+      end
+
+    end
+  end
+
+  [true, 'true'].each do |pval|
+    describe "with plugin param #{pval} (#{pval.class})" do
+      let (:params) {{ :config_filename => 'foo.xml', :changes => ['set foo bar'], :plugin => pval }}
+      it do
+        should contain_augeas('jenkins::augeas: myplug').with(
+          :incl    => "/var/lib/jenkins/foo.xml",
+          :context => "/files/var/lib/jenkins/foo.xml/",
+          :changes => ['set foo bar'],
+          :lens    => 'Xml.lns',
+        )
+        should contain_jenkins__plugin('myplug')
+      end
+    end
+  end
+
+  describe "with plugin param 'pluginname'" do
+    let (:params) {{ :config_filename => 'foo.xml', :changes => ['set foo bar'], :plugin => 'pluginname' }}
+    it do
+      should contain_augeas('jenkins::augeas: myplug').with(
+        :incl    => "/var/lib/jenkins/foo.xml",
+        :context => "/files/var/lib/jenkins/foo.xml/",
+        :changes => ['set foo bar'],
+        :lens    => 'Xml.lns',
+      )
+      should contain_jenkins__plugin('pluginname')
+    end
+  end
+
+  describe "with plugin_version set" do
+    let (:params) {{
+        :config_filename => 'foo.xml',
+        :changes         => [],
+        :plugin_version  => '0.1',
+        :plugin          => true,
+    }}
+    it do
+      should contain_jenkins__plugin('myplug').with(
+        :version       => '0.1',
+        :manage_config => false,
+      )
+    end
+  end
+  describe "with context set" do
+    let (:params) {{
+      :plugin          => false,
+      :config_filename => 'foo.xml',
+      :context         => 'foo/bar',
+      :changes         => [],
+    }}
+    it do
+      should contain_augeas('jenkins::augeas: myplug').with(
+        :incl    => '/var/lib/jenkins/foo.xml',
+        :context => '/files/var/lib/jenkins/foo.xml/foo/bar',
+        :lens    => 'Xml.lns',
+      )
+    end
+  end
+
+end
