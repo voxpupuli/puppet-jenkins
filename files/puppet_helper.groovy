c6fc4b44a0be93a962bcac9d66c7e5dc0c7584a5
Joshua Hoblitt <josh@hoblitt.com>
2016-03-29 13:23:35 -0700

    remove [oddly] duplicated import statements

--- files/puppet_helper.groovy
+++ files/puppet_helper.groovy
@@ -16,22 +16,18 @@
 // limitations under the License.
 
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
-import com.cloudbees.jenkins.plugins.sshcredentials.impl.*;
 import com.cloudbees.plugins.credentials.*
-import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
-import com.cloudbees.plugins.credentials.domains.*;
 import com.cloudbees.plugins.credentials.impl.*
-import com.cloudbees.plugins.credentials.impl.*;
-import hudson.plugins.sshslaves.*;
-import jenkins.model.*;
-import org.jenkinsci.plugins.*;
-import jenkins.security.*;
-import hudson.util.*;
-import hudson.model.*;
 import groovy.transform.InheritConstructors
+import hudson.model.*
+import hudson.plugins.sshslaves.*
+import hudson.util.*
+import jenkins.model.*
+import jenkins.security.*
 import org.apache.commons.io.IOUtils
+import org.jenkinsci.plugins.*
 
 class InvalidAuthenticationStrategy extends Exception{}
 @InheritConstructors
