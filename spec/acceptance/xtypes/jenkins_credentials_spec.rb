ba9ef01bb66dfe716e794fc44e12a32159d08cdf
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2016-05-19 09:21:23 +0200

    try another testing method

--- spec/acceptance/xtypes/jenkins_credentials_spec.rb
+++ spec/acceptance/xtypes/jenkins_credentials_spec.rb
@@ -32,6 +32,33 @@ describe 'jenkins_credentials' do
         end
       end
 
+      context 'UsernamePasswordCredentialsImpl' do
+        it 'should work with no errors' do
+          pp = base_manifest + <<-EOS
+            jenkins_credentials { '9b07d668-a87e-4877-9407-ae05056e32ac':
+              ensure      => 'present',
+              description => 'foo',
+              domain      => undef,
+              impl        => 'UsernamePasswordCredentialsImpl',
+              password    => 'password',
+              scope       => 'GLOBAL',
+              username    => 'bruce',
+            }
+          EOS
+
+          # Run it twice and test for idempotency
+          apply_manifest(pp, :catch_failures => true)
+          apply_manifest(pp, :catch_failures => true)
+        end
+
+        describe file('/var/lib/jenkins/credentials.xml') do
+          # XXX need to properly compare the XML doc
+          # trying to match anything other than the id this way might match other
+          # credentails
+          it { should contain '<username>bruce</username>' }
+        end
+      end
+
       context 'BasicSSHUserPrivateKey' do
         it 'should work with no errors' do
           pp = base_manifest + <<-EOS
@@ -59,36 +86,6 @@ describe 'jenkins_credentials' do
           it { should contain '<id>a0469025-1202-4007-983d-0c62f230f1a7</id>' }
         end
       end
-
-      context 'StringCredentialsImpl' do
-        it 'should work with no errors' do
-          pp = base_manifest + <<-EOS
-            jenkins::plugin { 'plain-credentials':
-              pin => true,
-            }
-
-            jenkins_credentials { '150b2895-b0eb-4813-b8a5-3779690c063c':
-              ensure      => 'present',
-              description => 'baz',
-              domain      => undef,
-              impl        => 'StringCredentialsImpl',
-              scope       => 'SYSTEM',
-              secret      => 'fluffy bunny',
-              username    => 'bruce',
-            }
-          EOS
-
-          # Run it twice and test for idempotency
-          apply_manifest(pp, :catch_failures => true)
-          apply_manifest(pp, :catch_failures => true)
-        end
-
-        describe file('/var/lib/jenkins/credentials.xml') do
-          # XXX need to properly compare the XML doc
-          # trying to match anything other than the id this way might match other
-          # credentails
-          it { should contain '<username>bruce</username>'}
-        end
 
       context 'StringCredentialsImpl' do
         it 'should work with no errors' do
