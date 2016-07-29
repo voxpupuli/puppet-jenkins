07580eff2b7b9b7977d5d16638a2b8f6ca8aed00
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-07-29 16:31:37 +0200

    default javabin in params is java, not /usr/bin/java

--- spec/classes/jenkins_config_spec.rb
+++ spec/classes/jenkins_config_spec.rb
@@ -10,35 +10,32 @@ describe 'jenkins', :type => :module do
         :operatingsystemmajrelease => '6',
       }
     end
+
     context 'config' do
       context 'default' do
         it { should contain_class('jenkins::config') }
-      end
-
-  context 'config' do
-    context 'default' do
-      it { should contain_class('jenkins::config') }
-      it { should contain_jenkins__plugin('credentials') }
-      it do
-        should contain_jenkins__sysconfig('JENKINS_JAVA_OPTIONS')
-          .with_value('-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false')
-      end
-      it do
-        should contain_jenkins__sysconfig('JENKINS_AJP_PORT').with_value('-1')
+        it { should contain_jenkins__plugin('credentials') }
+        it do
+          should contain_jenkins__sysconfig('JENKINS_JAVA_OPTIONS')
+            .with_value('-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false')
+        end
+        it do
+          should contain_jenkins__sysconfig('JENKINS_AJP_PORT').with_value('-1')
+        end
       end
     end
-  end
 
-  context 'on OpenBSD' do
-    let(:facts) { { :osfamily => 'OpenBSD', :operatingsystem => 'OpenBSD' } }
-    context 'config' do
-      context 'default' do
-        it { should contain_class('jenkins::config') }
-      end
-      context 'create config' do
-        let(:params) { { :config_hash => { 'AJP_PORT' => { 'value' => '1234' } } }}
-        it 'should fail' do
-          expect { should compile }.to raise_error
+    context 'on OpenBSD' do
+      let(:facts) { { :osfamily => 'OpenBSD', :operatingsystem => 'OpenBSD' } }
+      context 'config' do
+        context 'default' do
+          it { should contain_class('jenkins::config') }
+        end
+        context 'create config' do
+          let(:params) { { :config_hash => { 'AJP_PORT' => { 'value' => '1234' } } }}
+          it 'should fail' do
+            expect { should compile }.to raise_error
+          end
         end
       end
     end
