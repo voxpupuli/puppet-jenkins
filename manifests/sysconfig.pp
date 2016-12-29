ec0a6eb38fa5267eaf86ba859450d9ca4147ff97
Joshua Hoblitt <josh@hoblitt.com>
2016-12-29 15:08:40 -0700

    warn about sysconfig values that contain '$'
    Systemd does not support variable interpolation in an
    `EnvironmentFile`.

--- manifests/sysconfig.pp
+++ manifests/sysconfig.pp
@@ -5,6 +5,10 @@ define jenkins::sysconfig(
 ) {
   validate_string($value)
 
+  if ($value =~ /\$/) {
+    warning("Jenkins::Sysconfig[${name}]: detected \'\$\' in value -- be advised the variable interpolation will not work under systemd")
+  }
+
   file_line { "Jenkins sysconfig setting ${name}":
     path   => "${::jenkins::sysconfdir}/jenkins",
     line   => "${name}=\"${value}\"",
