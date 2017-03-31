dabd8e562848b1bb3eda981d4ccdaa6974504767
Jan Vansteenkiste <jan@vstone.eu>
2017-03-31 07:57:37 +0200

    Fix all require statements for files in puppet_x
    See: https://tickets.puppetlabs.com/browse/SERVER-973 for more
    information

--- lib/puppet/type/jenkins_authorization_strategy.rb
+++ lib/puppet/type/jenkins_authorization_strategy.rb
@@ -1,4 +1,4 @@
-require 'puppet_x/jenkins/type/cli'
+require_relative '../../puppet_x/jenkins/type/cli'
 
 PuppetX::Jenkins::Type::Cli.newtype(:jenkins_authorization_strategy) do
   @doc = "Manage Jenkins' authorization strategy"
