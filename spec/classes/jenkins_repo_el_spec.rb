dd8592212000551fe213c78475375686e0dc2607
R. Tyler Croy <tyler@monkeypox.org>
2017-06-20 10:28:51 -0700

    Default Red Hat family systems to HTTPs for their repositories

--- spec/classes/jenkins_repo_el_spec.rb
+++ spec/classes/jenkins_repo_el_spec.rb
@@ -13,20 +13,20 @@ describe 'jenkins', :type => :module do
 
   context 'repo::el' do
     describe 'default' do
-      it { should contain_yumrepo('jenkins').with_baseurl('http://pkg.jenkins-ci.org/redhat-stable/') }
+      it { should contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
       it { should contain_yumrepo('jenkins').with_proxy(nil) }
     end
 
     describe 'lts = true' do
       let(:params) { { :lts => true } }
-      it { should contain_yumrepo('jenkins').with_baseurl('http://pkg.jenkins-ci.org/redhat-stable/') }
+      it { should contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
       it { should contain_yumrepo('jenkins').with_proxy(nil) }
     end
 
     describe 'lts = false' do
       let(:params) { { :lts => false } }
       it { should contain_yumrepo('jenkins').with_proxy(nil) }
-      it { should contain_yumrepo('jenkins').with_baseurl('http://pkg.jenkins-ci.org/redhat/') }
+      it { should contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat/') }
     end
 
     describe 'repo_proxy is set' do
