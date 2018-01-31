39abc56008e3f4bcf5df9e54ba04ffb56ce2cb5a
Alexander Fisher <alex@linfratech.co.uk>
2018-01-31 19:17:31 +0000

    Rubocop: Fix Layout/AlignHash

--- spec/unit/jenkins_plugins_spec.rb
+++ spec/unit/jenkins_plugins_spec.rb
@@ -160,7 +160,7 @@ Plugin-Developers: Kohsuke Kawaguchi:kohsuke:,Nicolas De Loof:ndeloof:
 
   let(:git_plugin) do
     {'buildDate' => 'Jan 08, 2014',
-    'dependencies' =>
+     'dependencies' =>
       [{'name' => 'promoted-builds', 'optional' => true, 'version' => '2.7'},
       {'name' => 'token-macro', 'optional' => true, 'version' => '1.5.1'},
       {'name' => 'ssh-credentials', 'optional' => false, 'version' => '1.5.1'},
@@ -169,25 +169,25 @@ Plugin-Developers: Kohsuke Kawaguchi:kohsuke:,Nicolas De Loof:ndeloof:
       {'name' => 'multiple-scms', 'optional' => true, 'version' => '0.2'},
       {'name' => 'parameterized-trigger', 'optional' => true, 'version' => '2.4'},
       {'name' => 'git-client', 'optional' => false, 'version' => '1.6.0'}],
-    'developers' =>
+     'developers' =>
       [{'developerId' => 'kohsuke', 'name' => 'Kohsuke Kawaguchi'},
       {'developerId' => 'ndeloof',
-        'email' => 'nicolas.deloof@gmail.com',
-        'name' => 'Nicolas De Loof'}],
-    'excerpt' =>
+       'email' => 'nicolas.deloof@gmail.com',
+       'name' => 'Nicolas De Loof'}],
+     'excerpt' =>
       "This plugin allows use of <a href='http://git-scm.com/'>Git</a> as a build SCM. A recent Git runtime is required (1.7.9 minimum, 1.8.x recommended). Plugin is only tested on official <a href='http://git-scm.com/'>git client</a>. Use exotic installations at your own risks.",
-    'gav' => 'org.jenkins-ci.plugins:git:2.0.1',
-    'labels' => ['scm'],
-    'name' => 'git',
-    'previousTimestamp' => '2013-10-22T22:00:16.00Z',
-    'previousVersion' => '2.0',
-    'releaseTimestamp' => '2014-01-08T21:46:20.00Z',
-    'requiredCore' => '1.480',
-    'scm' => 'github.com',
-    'sha1' => 'r5bK/IP8soP08D55Xpcx5yWHzdY=',
-    'title' => 'Git Plugin',
-    'url' => 'http://updates.jenkins-ci.org/download/plugins/git/2.0.1/git.hpi',
-    'version' => '2.0.1',
-    'wiki' => 'https://wiki.jenkins-ci.org/display/JENKINS/Git+Plugin'}
+     'gav' => 'org.jenkins-ci.plugins:git:2.0.1',
+     'labels' => ['scm'],
+     'name' => 'git',
+     'previousTimestamp' => '2013-10-22T22:00:16.00Z',
+     'previousVersion' => '2.0',
+     'releaseTimestamp' => '2014-01-08T21:46:20.00Z',
+     'requiredCore' => '1.480',
+     'scm' => 'github.com',
+     'sha1' => 'r5bK/IP8soP08D55Xpcx5yWHzdY=',
+     'title' => 'Git Plugin',
+     'url' => 'http://updates.jenkins-ci.org/download/plugins/git/2.0.1/git.hpi',
+     'version' => '2.0.1',
+     'wiki' => 'https://wiki.jenkins-ci.org/display/JENKINS/Git+Plugin'}
   end
 end
