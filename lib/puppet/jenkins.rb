530c7c52faef673b0445e7028f5913a7b9e047e4
Alexander Fisher <alex@linfratech.co.uk>
2018-02-07 19:32:15 +0000

    Rubocop: Fix Style/RedundantBegin

--- lib/puppet/jenkins.rb
+++ lib/puppet/jenkins.rb
@@ -3,12 +3,10 @@ module Puppet
   module Jenkins
     # @return [String] Full path to the Jenkins user's home directory
     def self.home_dir
-      begin
-        return File.expand_path('~jenkins')
-      rescue ArgumentError
-        # The Jenkins user doesn't exist!
-        return nil
-      end
+      return File.expand_path('~jenkins')
+    rescue ArgumentError
+      # The Jenkins user doesn't exist!
+      return nil
     end
 
     # @return [String] Full path to the Jenkins user's plugin directory
