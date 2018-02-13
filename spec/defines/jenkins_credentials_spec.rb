6ac05d45f61f7fb9441fadb1a27fe1415aea0c2f
Alexander Fisher <alex@linfratech.co.uk>
2018-02-13 19:48:44 +0000

    Rubocop: Fix Layout/SpaceBeforeComma

--- spec/defines/jenkins_credentials_spec.rb
+++ spec/defines/jenkins_credentials_spec.rb
@@ -35,7 +35,7 @@ describe 'jenkins::credentials', type: :define do
     }
     end
 
-    it { is_expected.to contain_jenkins__cli__exec('create-jenkins-credentials-foo').with(command: ['create_or_update_credentials' , title.to_s, "'mypass'",
+    it { is_expected.to contain_jenkins__cli__exec('create-jenkins-credentials-foo').with(command: ['create_or_update_credentials', title.to_s, "'mypass'",
                                                                                                     "''", "'Managed by Puppet'", "''"],
                                                                                           unless: "for i in \$(seq 1 10); do \$HELPER_CMD credential_info #{title} && break || sleep 10; done | grep #{title}")
     }
@@ -59,7 +59,7 @@ describe 'jenkins::credentials', type: :define do
     }
     end
 
-    it { is_expected.to contain_jenkins__cli__exec('create-jenkins-credentials-foo').with(command: ['create_or_update_credentials' , title.to_s, "'mypass'",
+    it { is_expected.to contain_jenkins__cli__exec('create-jenkins-credentials-foo').with(command: ['create_or_update_credentials', title.to_s, "'mypass'",
                                                                                                     "'e94d3b98-5ba4-43b9-89ed-79a08ea97f6f'", "'Managed by Puppet'", "''"],
                                                                                           unless: "for i in \$(seq 1 10); do \$HELPER_CMD credential_info #{title} && break || sleep 10; done | grep #{title}")
     }
