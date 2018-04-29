ab7f8e3528b7065597ed188f6df3593c1160f710
Tim Meusel <tim@bastelfreak.de>
2018-04-29 16:15:39 +0200

    fix spec errors in jenkins_repo_el_spec.rb

--- spec/classes/jenkins_repo_el_spec.rb
+++ spec/classes/jenkins_repo_el_spec.rb
@@ -1,40 +1,46 @@
 require 'spec_helper'
 
 describe 'jenkins', type: :class do
-  # Switching OS Family to prevent duplicate declaration
-  let(:facts) do
-    {
-      osfamily: 'Redhat',
-      operatingsystem: 'CentOS',
-      operatingsystemrelease: '6.7',
-      operatingsystemmajrelease: '6'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    next unless facts[:os]['family'] == 'RedHat'
 
-  context 'repo::el' do
-    describe 'default' do
-      it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
-      it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
-    end
+    context "on #{os} " do
+      systemd_fact = case facts[:operatingsystemmajrelease]
+                     when '6'
+                       { systemd: false }
+                     else
+                       { systemd: true }
+                     end
+      let :facts do
+        facts.merge(systemd_fact)
+      end
 
-    describe 'lts = true' do
-      let(:params) { { lts: true } }
+      context 'repo::el' do
+        describe 'default' do
+          it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
+          it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
+        end
 
-      it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
-      it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
-    end
+        describe 'lts = true' do
+          let(:params) { { lts: true } }
 
-    describe 'lts = false' do
-      let(:params) { { lts: false } }
+          it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat-stable/') }
+          it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
+        end
 
-      it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
-      it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat/') }
-    end
+        describe 'lts = false' do
+          let(:params) { { lts: false } }
 
-    describe 'repo_proxy is set' do
-      let(:params) { { repo_proxy: 'http://proxy:8080/' } }
+          it { is_expected.to contain_yumrepo('jenkins').with_proxy(nil) }
+          it { is_expected.to contain_yumrepo('jenkins').with_baseurl('https://pkg.jenkins.io/redhat/') }
+        end
 
-      it { is_expected.to contain_yumrepo('jenkins').with_proxy('http://proxy:8080/') }
+        describe 'repo_proxy is set' do
+          let(:params) { { repo_proxy: 'http://proxy:8080/' } }
+
+          it { is_expected.to contain_yumrepo('jenkins').with_proxy('http://proxy:8080/') }
+        end
+      end
     end
   end
 end
