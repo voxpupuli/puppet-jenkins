0797caeac0eb0e77f4fe80bd9dfcdb345ab8565c
Tim Meusel <tim@bastelfreak.de>
2018-04-29 15:59:14 +0200

    fix spec errors in jenkins_sysconfig_spec.rb

--- spec/defines/jenkins_sysconfig_spec.rb
+++ spec/defines/jenkins_sysconfig_spec.rb
@@ -1,50 +1,45 @@
 require 'spec_helper'
 
 describe 'jenkins::sysconfig' do
-  let(:pre_condition) { 'include ::jenkins' }
+  let(:pre_condition) { 'include jenkins' }
 
   let(:title) { 'myprop' }
+
   let(:params) { { 'value' => 'myvalue' } }
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      systemd_fact = case facts[:operatingsystemmajrelease]
+                     when '6'
+                       { systemd: false }
+                     else
+                       { systemd: true }
+                     end
+      let :facts do
+        facts.merge(systemd_fact)
+      end
 
-  describe 'on RedHat' do
-    let(:facts) do
-      {
-        osfamily: 'RedHat',
-        operatingsystem: 'CentOS',
-        operatingsystemrelease: '7.2',
-        operatingsystemmajrelease: '7'
-      }
+      case facts[:os]['family']
+      when 'RedHat'
+        describe 'on RedHat' do
+          it do
+            is_expected.to contain_file_line('Jenkins sysconfig setting myprop').with(
+              path: '/etc/sysconfig/jenkins',
+              line: 'myprop="myvalue"',
+              match: '^myprop='
+            ).that_notifies('Service[jenkins]')
+          end
+        end
+      when 'Debian'
+        describe 'on Debian' do
+          it do
+            is_expected.to contain_file_line('Jenkins sysconfig setting myprop').with(
+              path: '/etc/default/jenkins',
+              line: 'myprop="myvalue"',
+              match: '^myprop='
+            ).that_notifies('Service[jenkins]')
+          end
+        end
+      end
     end
-
-    it do
-      is_expected.to contain_file_line('Jenkins sysconfig setting myprop').with(
-        path: '/etc/sysconfig/jenkins',
-        line: 'myprop="myvalue"',
-        match: '^myprop='
-      ).that_notifies('Service[jenkins]')
-    end
-  end # on RedHat
-
-  describe 'on Debian' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        operatingsystem: 'Debian',
-        lsbdistcodename: 'squeeze',
-        lsbdistid: 'debian',
-        os: {
-          name: 'Debian',
-          release: { full: '11.04' }
-        }
-      }
-    end
-
-    it do
-      is_expected.to contain_file_line('Jenkins sysconfig setting myprop').with(
-        path: '/etc/default/jenkins',
-        line: 'myprop="myvalue"',
-        match: '^myprop='
-      ).that_notifies('Service[jenkins]')
-    end
-  end # on Debian
+  end
 end
