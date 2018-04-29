04483722b7ebea83259760ea59a605d6ba45d959
Tim Meusel <tim@bastelfreak.de>
2018-04-29 14:38:31 +0200

    fix spec errors in jenkins_user_spec.rb

--- spec/defines/jenkins_user_spec.rb
+++ spec/defines/jenkins_user_spec.rb
@@ -2,25 +2,37 @@ require 'spec_helper'
 
 describe 'jenkins::user', type: :define do
   let(:title) { 'foo' }
-  let(:facts) do
-    {
-      osfamily: 'RedHat',
-      operatingsystem: 'RedHat',
-      operatingsystemrelease: '6.7',
-      operatingsystemmajrelease: '6'
-    }
+
+
+  let :pre_condition do
+    'include jenkins'
   end
 
-  describe 'relationships' do
-    let(:params) { { email: 'foo@example.org', password: 'foo' } }
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
 
-    it do
-      is_expected.to contain_jenkins__user('foo').
-        that_requires('Class[jenkins::cli_helper]')
-    end
-    it do
-      is_expected.to contain_jenkins__user('foo').
-        that_comes_before('Anchor[jenkins::end]')
+      systemd_fact = case facts[:operatingsystemmajrelease]
+                     when '6'
+                       { systemd: false }
+                     else
+                       { systemd: true }
+                     end
+      let :facts do
+        facts.merge(systemd_fact)
+      end
+
+      describe 'relationships' do
+        let(:params) { { email: 'foo@example.org', password: 'foo' } }
+
+        it do
+          is_expected.to contain_jenkins__user('foo').
+            that_requires('Class[jenkins::cli_helper]')
+        end
+        it do
+          is_expected.to contain_jenkins__user('foo').
+            that_comes_before('Anchor[jenkins::end]')
+        end
+      end
     end
   end
 end
