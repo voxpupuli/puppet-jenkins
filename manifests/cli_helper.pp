f4770a7c28688ef2147f15564dc48386b85801e5
Tim Meusel <tim@bastelfreak.de>
2018-04-29 16:03:40 +0200

    fix spec errors in jenkins_cli_helper_spec.rb

--- manifests/cli_helper.pp
+++ manifests/cli_helper.pp
@@ -2,39 +2,26 @@
 #
 # A helper script for creating resources via the Jenkins cli
 #
-# Parameters:
-#
-# ssh_keyfile = undef
-#   Defaults to the value of $::jenkins::cli_ssh_keyfile. This parameter is
-#   deprecated, please set $::jenkins::cli_ssh_keyfile instead of setting this
-#   directly
-#
-class jenkins::cli_helper (
-  Optional[Stdlib::Absolutepath] $ssh_keyfile = $::jenkins::cli_ssh_keyfile,
-) {
-  include ::jenkins
-  include ::jenkins::cli
+class jenkins::cli_helper {
+  include jenkins
+  include jenkins::cli
 
   Class['jenkins::cli']
-    -> Class['jenkins::cli_helper']
-      -> Anchor['jenkins::end']
+  -> Class['jenkins::cli_helper']
+  -> Anchor['jenkins::end']
 
-  $libdir = $::jenkins::libdir
-  $cli_jar = $::jenkins::cli::jar
+  $libdir = $jenkins::libdir
+  $cli_jar = $jenkins::cli::jar
   $port = jenkins_port()
   $prefix = jenkins_prefix()
   $helper_groovy = "${libdir}/puppet_helper.groovy"
 
   file { $helper_groovy:
     source  => 'puppet:///modules/jenkins/puppet_helper.groovy',
-    owner   => $::jenkins::user,
-    group   => $::jenkins::group,
+    owner   => $jenkins::user,
+    group   => $jenkins::group,
     mode    => '0444',
     require => Class['jenkins::cli'],
-  }
-
-  if $ssh_keyfile != $::jenkins::cli_ssh_keyfile {
-    info("Using jenkins::cli_helper(${ssh_keyfile}) is deprecated and will be removed in the next major version of this module")
   }
 
   $helper_cmd = join(
