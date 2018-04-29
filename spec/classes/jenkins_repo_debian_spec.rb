902a064b79fa7d06eafb56d60fe12789a1552f54
Tim Meusel <tim@bastelfreak.de>
2018-04-29 16:14:12 +0200

    fix spec errors in jenkins_repo_debian_spec.rb

--- spec/classes/jenkins_repo_debian_spec.rb
+++ spec/classes/jenkins_repo_debian_spec.rb
@@ -1,43 +1,47 @@
 require 'spec_helper'
 
 describe 'jenkins', type: :class do
-  # Switching OS Family to prevent duplicate declaration
-  let(:facts) do
-    {
-      osfamily: 'Debian',
-      lsbdistcodename: 'precise',
-      lsbdistid: 'ubuntu',
-      operatingsystem: 'Debian',
-      os: {
-        name: 'Debian',
-        release: { full: '11.04' }
-      }
-    }
-  end
+  on_supported_os.each do |os, facts|
 
-  context 'repo::debian' do
-    shared_examples 'an apt catalog' do
-      it { is_expected.to contain_class('apt') }
-      it { is_expected.to contain_apt__source('jenkins').that_notifies('Exec[apt_update]') }
-    end
+    next unless facts[:os]['family'] == 'Debian'
 
-    describe 'default' do
-      it_behaves_like 'an apt catalog'
-      it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian-stable') }
-    end
+    context "on #{os} " do
+      systemd_fact = case facts[:operatingsystemmajrelease]
+                     when '6'
+                       { systemd: false }
+                     else
+                       { systemd: true }
+                     end
+      let :facts do
+        facts.merge(systemd_fact)
+      end
 
-    describe 'lts = true' do
-      let(:params) { { lts: true } }
 
-      it_behaves_like 'an apt catalog'
-      it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian-stable') }
-    end
+      context 'repo::debian' do
+        shared_examples 'an apt catalog' do
+          it { is_expected.to contain_class('apt') }
+          it { is_expected.to contain_apt__source('jenkins').that_notifies('Exec[apt_update]') }
+        end
 
-    describe 'lts = false' do
-      let(:params) { { lts: false } }
+        describe 'default' do
+          it_behaves_like 'an apt catalog'
+          it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian-stable') }
+        end
 
-      it_behaves_like 'an apt catalog'
-      it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian') }
+        describe 'lts = true' do
+          let(:params) { { lts: true } }
+
+          it_behaves_like 'an apt catalog'
+          it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian-stable') }
+        end
+
+        describe 'lts = false' do
+          let(:params) { { lts: false } }
+
+          it_behaves_like 'an apt catalog'
+          it { is_expected.to contain_apt__source('jenkins').with_location('https://pkg.jenkins.io/debian') }
+        end
+      end
     end
   end
 end
