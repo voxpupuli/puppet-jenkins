f69e3184e33ec8675372b2c1111dd41eeb52e43a
Nutting, Alex <alex.nutting@hasbro.com>
2019-01-11 08:49:06 -0500

    Change slave on mac to use plist and use EPP

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -214,17 +214,9 @@ class jenkins::slave (
       $defaults_group   = 'wheel'
       $manage_user_home = false
 
-      file { "${slave_home}/start-slave.sh":
-        ensure  => 'file',
-        content => template("${module_name}/start-slave.sh.erb"),
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'wheel',
-      }
-
       file { '/Library/LaunchDaemons/org.jenkins-ci.slave.jnlp.plist':
         ensure  => 'file',
-        content => template("${module_name}/org.jenkins-ci.slave.jnlp.plist.erb"),
+        content => template("${module_name}/org.jenkins-ci.slave.jnlp.plist.epp"),
         mode    => '0644',
         owner   => 'root',
         group   => 'wheel',
