2365ebcc23e05104b313600f5dfa5dbf082f314f
Jan Vansteenkiste <jan@vstone.eu>
2019-06-05 11:28:12 +0200

    Split out user-creation logic
    By splitting out the user creation, we can have a custom
    localdir before the package (maybe) tries to create it or adjust it.

---
+++ manifests/user_setup.pp
@@ -0,0 +1,60 @@
+# This class should be considered private
+#
+# It will optionally create the jenkins user and make sure all
+# directories have proper permissions setup.
+#
+# By having this in a separate class that is managed before
+# installing the package, we can effectivly override the
+# default local dir that is otherwise possibly created by
+# the package.
+#
+class jenkins::user_setup {
+
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
+  $dir_params = {
+    ensure => directory,
+    owner  => $::jenkins::user,
+    group  => $::jenkins::group,
+    mode   => '0755',
+  }
+
+  # ensure_resource is used to try to maintain backwards compatiblity with
+  # manifests that were able to external declare resources due to the
+  # old conditional behavior of jenkins::plugin
+  if $::jenkins::manage_user {
+    ensure_resource('user', $::jenkins::user, {
+      ensure     => present,
+      gid        => $::jenkins::group,
+      home       => $::jenkins::localstatedir,
+      managehome => false,
+      system     => true,
+    })
+  }
+
+  if $::jenkins::manage_group {
+    ensure_resource('group', $::jenkins::group, {
+      ensure => present,
+      system => true,
+    })
+  }
+
+  $plugin_dir_params = $::jenkins::purge_plugins ? {
+    true    => $dir_params + {
+      'purge'   => true,
+      'recurse' => true,
+      'force'   => true,
+      'notify'  => Service['jenkins'],
+    },
+    default => $dir_params,
+  }
+
+  if $::jenkins::manage_datadirs {
+    ensure_resource('file', $::jenkins::localstatedir, $dir_params)
+    ensure_resource('file', $::jenkins::plugin_dir, $plugin_dir_params)
+    ensure_resource('file', $::jenkins::job_dir, $dir_params)
+  }
+
+}
