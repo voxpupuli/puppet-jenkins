cf897480b864e3df80f5ef2e255b26cce9d4d745
Pajkanon <Jocce@jocce.nu>
2020-02-04 12:36:44 +0100

    Update cli.pp

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -39,6 +39,7 @@ class jenkins::cli {
     path        => ['/bin', '/usr/bin'],
     cwd         => '/tmp',
     refreshonly => true,
+    require     => Service['jenkins'],
   }
   # Extract latest CLI in case package is updated / downgraded
   Package[$jenkins::package_name] ~> Exec['jenkins-cli']
