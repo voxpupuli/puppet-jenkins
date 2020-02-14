2fcd367c71fbc825615d5a9589135265cb9868c3
gbrandyb <42916741+gbrandyb@users.noreply.github.com>
2020-02-14 16:59:52 -0500

    change 'require' to 'require_relative'
    on 2019.2 PE, facter is throwing an error on Windows nodes -> "error while resolving custom facts in C:/ProgramData\PuppetLabs\puppet\cache\lib\facter\jenkins.rb: cannot load such file -- puppet/jenkins/facts

--- lib/facter/jenkins.rb
+++ lib/facter/jenkins.rb
@@ -4,7 +4,7 @@
 # jenkins plugins + versions.
 #
 #
-require 'puppet/jenkins/facts'
+require_relative '../puppet/jenkins/facts'
 
 # If we're being loaded inside the module, we'll need to go ahead and add our
 # facts then won't we?
