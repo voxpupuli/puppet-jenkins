f41aeb2b9dd829ce830ec66a5c592fc41e9ec9d9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-02-22 16:44:13 +0100

    Simplify the fact definition
    This removes the entire module for the facts. The benefit is that it's
    much easier to see what's going on. There is still testing which means
    there's no reduction in code coverage.

--- lib/facter/jenkins.rb
+++ lib/facter/jenkins.rb
@@ -2,10 +2,13 @@
 #
 # Creates a fact 'jenkins_plugins' containing a comma-delimited string of all
 # jenkins plugins + versions.
-#
-#
-require 'puppet/jenkins/facts'
+require 'facter'
+require_relative '../puppet/jenkins/plugins'
 
-# If we're being loaded inside the module, we'll need to go ahead and add our
-# facts then won't we?
-Puppet::Jenkins::Facts.install
+Facter.add(:jenkins_plugins) do
+  confine kernel: 'Linux'
+  setcode do
+    plugins = Puppet::Jenkins::Plugins.available
+    plugins.keys.sort.map { |plugin| "#{plugin} #{plugins[plugin][:plugin_version]}" }.join(', ')
+  end
+end
