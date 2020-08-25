5770de22333846d3640e613e3b0bf3115b6d7c00
Matt Wade <matthew.wade@ioppublishing.org>
2020-08-25 10:42:48 +0100

    upd

--- manifests/repo/el.pp
+++ manifests/repo/el.pp
@@ -6,14 +6,14 @@ class jenkins::repo::el {
   if $jenkins::lts {
     $baseurl = "${jenkins::repo::base_url}/redhat-stable/"
   } else {
-    $baseurl = "${jenkins::repo::base_url}/redhat/"
+    $baseurl = "${jenkins::repo::base_url}/redhat/${jenkins::repo::gpg_key_filename}"
   }
 
   yumrepo {'jenkins':
     descr    => 'Jenkins',
     baseurl  => $baseurl,
     gpgcheck => 1,
-    gpgkey   => "${baseurl}${jenkins::repo::gpg_key_filename}",
+    gpgkey   => "${baseurl}/redhat/${jenkins::repo::gpg_key_filename}",
     enabled  => 1,
     proxy    => $jenkins::repo_proxy,
   }
