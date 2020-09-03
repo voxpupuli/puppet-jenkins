206d578aa85a6899090cf6fb662871046c44ea39
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-09-03 17:56:32 +0200

    Remove use of validate_bool in example

--- examples/plugin-configuration/git.pp
+++ examples/plugin-configuration/git.pp
@@ -1,14 +1,13 @@
 # lint:ignore:autoloader_layout
 class jenkins::plugin::git (
 # lint:endignore
-  $version            = 0,
-  $config_filename    = 'hudson.plugins.git.GitSCM.xml',
-  $config_content     = undef,
-  $git_name           = 'Jenkins',
-  $git_email          = 'jenkins@example.net',
-  $git_create_account = false,) {
-  validate_bool($git_create_account)
-
+  $version                    = 0,
+  $config_filename            = 'hudson.plugins.git.GitSCM.xml',
+  $config_content             = undef,
+  $git_name                   = 'Jenkins',
+  $git_email                  = 'jenkins@example.net',
+  Boolean $git_create_account = false,
+) {
   if $config_content == undef {
     $real_content = template('jenkins/plugin/git.config.xml.erb')
   } else {
