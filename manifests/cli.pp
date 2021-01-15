12ccb382cc8b0a6e734cfda79b3b33003831fbe2
Bradon Kanyid <bradon.kanyid@blueowl.xyz>
2021-01-15 13:33:42 -0800

    Newer jenkins cli is located ... elsewhere

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -25,8 +25,8 @@ class jenkins::cli {
   }
 
   $jar = "${jenkins::libdir}/jenkins-cli.jar"
-  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/jenkins-cli.jar"
-  $move_jar = "mv WEB-INF/jenkins-cli.jar ${jar}"
+  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/lib/"
+  $move_jar = "mv WEB-INF/lib/cli-*.jar ${jar}"
   $remove_dir = 'rm -rf WEB-INF'
 
   # make sure we always call Exec[jenlins-cli] in case
