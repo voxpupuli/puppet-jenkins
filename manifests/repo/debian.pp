bb77ffb7484146c85ddc6962b65bef86ac8555eb
Bradon Kanyid <bradon.kanyid@blueowl.xyz>
2021-01-15 12:22:51 -0800

    Fix lts/non-lts

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -9,43 +9,31 @@ class jenkins::repo::debian
   include ::stdlib
   include ::apt
 
+  if $jenkins::lts {
+    $location = "debian-stable"
+  } else {
+    $location = "debian"
+  }
+
   $pkg_host = 'https://pkg.jenkins.io'
   $pkg_key_id = '62A9756BFD780C377CF24BA8FCEF32E745F2C3D5'
-  $pkg_key_source = "${pkg_host}/debian/jenkins-io.key"
+  $pkg_key_source = "${pkg_host}/${location}/jenkins-io.key"
 
   ensure_packages(['apt-transport-https'])
 
-  if $::jenkins::lts  {
-    apt::source { 'jenkins':
-      location => "${pkg_host}/debian-stable",
-      release  => 'binary/',
-      repos    => '',
-      include  => {
-        'src' => false,
-      },
-      key      => {
-        'id'     => "${pkg_key_id}",
-        'source' => "${pkg_key_source}",
-      },
-      require  => Package['apt-transport-https'],
-      notify   => Exec['apt_update'],
-    }
-  }
-  else {
-    apt::source { 'jenkins':
-      location => "${pkg_host}/debian",
-      release  => 'binary/',
-      repos    => '',
-      include  => {
-        'src' => false,
-      },
-      key      => {
-        'id'     => "${pkg_key_id}",
-        'source' => "${pkg_key_source}",
-      },
-      require  => Package['apt-transport-https'],
-      notify   => Exec['apt_update'],
-    }
+  apt::source { 'jenkins':
+    location => "${pkg_host}/${location}",
+    release  => 'binary/',
+    repos    => '',
+    include  => {
+      'src' => false,
+    },
+    key      => {
+      'id'     => "${pkg_key_id}",
+      'source' => "${pkg_key_source}",
+    },
+    require  => Package['apt-transport-https'],
+    notify   => Exec['apt_update'],
   }
 
   anchor { 'jenkins::repo::debian::begin': }
