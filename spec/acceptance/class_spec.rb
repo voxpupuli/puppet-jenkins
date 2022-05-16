voxpupuli/puppet-jenkins

commit 98f46ca999635d0a9a2f4ad84d70ed60389f7b47

Move to native systemd integration
Jenkins 2.332 started to include a systemd unit file which means we can
greatly simplify this module. It aligns all distros on a single
standard.

No attempt is made at supporting Jenkins < 2.332 but the old override
file is cleaned up.

manifests/config.pp
@@ -9,5 +9,9 @@ class jenkins::config {
     $jenkins::params::config_hash_defaults,
     $jenkins::config_hash
   )
-  create_resources('jenkins::sysconfig', $config_hash)
+
+  systemd::dropin_file { 'puppet-overrides.conf':
+    unit    => 'jenkins.service',
+    content => epp("${module_name}/jenkins-override.epp", { 'environment' => $config_hash }),
+  }
 }

spec/acceptance/class_spec.rb
@@ -19,7 +19,7 @@ describe 'jenkins class' do
       }
     end
 
-    describe file("#{LIBDIR}/jenkins-cli.jar") do
+    describe file('/usr/share/java/jenkins-cli.jar') do
       it { is_expected.to be_file }
       it { is_expected.to be_readable.by('owner') }
       it { is_expected.to be_writable.by('owner') }
@@ -27,38 +27,14 @@ describe 'jenkins class' do
       it { is_expected.to be_readable.by('others') }
     end
 
-    describe file("#{SYSCONFDIR}/jenkins") do
+    describe file('/etc/systemd/system/jenkins.service.d/puppet-overrides.conf') do
       it { is_expected.to be_file }
-      if fact('osfamily') == 'Debian'
-        it { is_expected.to contain 'AJP_PORT="-1"' }
-      else
-        it { is_expected.to contain 'JENKINS_AJP_PORT="-1"' }
-      end
+      it { is_expected.to contain 'Environment=' }
     end
 
     describe service('jenkins') do
       it { is_expected.to be_running }
       it { is_expected.to be_enabled }
-    end
-
-    if fact('osfamily') == 'RedHat' && SYSTEMD
-      describe file('/etc/systemd/system/jenkins.service') do
-        it { is_expected.to be_file }
-        it { is_expected.to contain "ExecStart=#{libdir}/jenkins-run" }
-      end
-      describe file('/etc/init.d/jenkins') do
-        it { is_expected.not_to exist }
-      end
-      describe service('jenkins') do
-        it { is_expected.to be_running.under('systemd') }
-      end
-    else
-      describe file('/etc/systemd/system/jenkins.service') do
-        it { is_expected.not_to exist }
-      end
-      describe file('/etc/init.d/jenkins') do
-        it { is_expected.to be_file }
-      end
     end
   end # default parameters
 

.fixtures.yml
@@ -6,7 +6,6 @@ fixtures:
     zypprepo: "https://github.com/voxpupuli/puppet-zypprepo.git"
     archive: "https://github.com/voxpupuli/puppet-archive.git"
     systemd: "https://github.com/voxpupuli/puppet-systemd"
-    transition: "https://github.com/puppetlabs/puppetlabs-transition"
     yumrepo_core:
       repo: https://github.com/puppetlabs/puppetlabs-yumrepo_core.git
       puppet_version: ">= 6.0.0"

lib/facter/jenkins.rb
@@ -12,25 +12,3 @@ Facter.add(:jenkins_plugins) do
     plugins.keys.sort.map { |plugin| "#{plugin} #{plugins[plugin][:plugin_version]}" }.join(', ')
   end
 end
-
-Facter.add(:jenkins_version) do
-  confine kernel: 'Linux'
-
-  setcode do
-    libdir = case Facter.value('os.family')
-             when 'Debian'
-               '/usr/share/jenkins'
-             when 'Archlinux'
-               '/usr/share/java/jenkins'
-             else
-               '/usr/lib/jenkins'
-             end
-    war = libdir + '/jenkins.war'
-
-    if Facter::Util::Resolution.which('java') and File.exist?(war)
-      Facter::Util::Resolution.exec(
-        'java -jar %s --version' % [war]
-      )
-    end
-  end
-end

lib/puppet/parser/functions/jenkins_port.rb
@@ -10,12 +10,6 @@ module Puppet::Parser::Functions
     ENDHEREDOC
 
     config_hash = lookupvar('::jenkins::config_hash')
-    if config_hash && \
-       config_hash['HTTP_PORT'] && \
-       config_hash['HTTP_PORT']['value']
-      return config_hash['HTTP_PORT']['value']
-    else
-      return 8080
-    end
+    config_hash&.dig('JENKINS_PORT', 'value') || 8080
   end
 end

lib/puppet/parser/functions/jenkins_prefix.rb
@@ -10,12 +10,6 @@ module Puppet::Parser::Functions
     ENDHEREDOC
 
     config_hash = lookupvar('::jenkins::config_hash')
-    if config_hash && \
-       config_hash['PREFIX'] && \
-       config_hash['PREFIX']['value']
-      return config_hash['PREFIX']['value']
-    else
-      return ''
-    end
+    config_hash&.dig('PREFIX', 'value') || ''
   end
 end

lib/puppet/x/jenkins/config.rb
@@ -11,10 +11,10 @@ class Puppet::X::Jenkins::Config
   class UnknownConfig < ArgumentError; end
 
   DEFAULTS = {
-    cli_jar: '/usr/lib/jenkins/jenkins-cli.jar',
+    cli_jar: '/usr/share/java/jenkins-cli.jar',
     url: 'http://localhost:8080',
     ssh_private_key: nil,
-    puppet_helper: '/usr/lib/jenkins/puppet_helper.groovy',
+    puppet_helper: '/usr/share/java/puppet_helper.groovy',
     cli_tries: 30,
     cli_try_sleep: 2,
     cli_username: nil,

manifests/init.pp
@@ -2,10 +2,9 @@
 # This class manages the [Jenkins CI/CD service](https://jenkins.io/index.html).
 #
 # Note that if different jenkins listening port(s) are configured via
-# ``jenkins::port``, ``jenkins::config_hash`` and/or a ``jenkins::sysconf``
-# resource, "bad things" are likely to happen.  This is a known implementation
-# problem with this module that can not be fixed without breaking backwards
-# compatibility.
+# ``jenkins::port`` and ``jenkins::config_hash`` resource, "bad things" are
+# likely to happen.  This is a known implementation problem with this module
+# that can not be fixed without breaking backwards compatibility.
 #
 # @param version
 #   package to install
@@ -79,8 +78,7 @@
 # @example Bulk sysconf
 #   class{ 'jenkins':
 #     config_hash => {
-#       'HTTP_PORT' => { 'value' => '9090' },
-#       'AJP_PORT'  => { 'value' => '9009' },
+#       'JENKINS_PORT' => { 'value' => '9090' },
 #     }
 #   }
 #
@@ -205,16 +203,6 @@
 # @param libdir
 #   Path to jenkins core files
 #
-#   * Redhat: ``/usr/lib/jenkins``
-#   * Debian: ``/usr/share/jenkins``
-#
-# @param sysconfdir
-#   Controls the path to the "sysconfig" file that stores jenkins service
-#   start-up variables
-#
-#   * RedHat: ``/etc/sysconfig/jenkins``
-#   * Debian: ``/etc/default/jenkins``
-#
 # @param manage_datadirs
 #   manage the local state dir, plugins dir and jobs dir
 #
@@ -282,9 +270,6 @@
 #       version: '2.16.0'
 #     # /support-core deps
 #
-# @param systemd_type
-#   Define a systemd unit type
-#
 class jenkins (
   String $version                                 = 'installed',
   Boolean $lts                                    = true,
@@ -296,7 +281,7 @@ class jenkins (
   Boolean $manage_service                         = true,
   Boolean $service_enable                         = true,
   Enum['running', 'stopped'] $service_ensure      = 'running',
-  Optional[String] $service_provider              = $jenkins::params::service_provider,
+  Optional[String] $service_provider              = undef,
   Hash $config_hash                               = {},
   Hash $plugin_hash                               = {},
   Hash $job_hash                                  = {},
@@ -315,8 +300,7 @@ class jenkins (
   Integer $cli_tries                              = 10,
   Integer $cli_try_sleep                          = 10,
   Integer $port                                   = 8080,
-  Stdlib::Absolutepath $libdir                    = $jenkins::params::libdir,
-  Stdlib::Absolutepath $sysconfdir                = $jenkins::params::sysconfdir,
+  Stdlib::Absolutepath $libdir                    = '/usr/share/java',
   Boolean $manage_datadirs                        = true,
   Stdlib::Absolutepath $localstatedir             = '/var/lib/jenkins',
   Optional[Integer] $executors                    = undef,
@@ -328,7 +312,6 @@ class jenkins (
   Array $default_plugins                          = $jenkins::params::default_plugins,
   String $default_plugins_host                    = 'https://updates.jenkins.io',
   Boolean $purge_plugins                          = false,
-  Enum['simple', 'forking'] $systemd_type         = $jenkins::params::systemd_type,
 ) inherits jenkins::params {
   if $purge_plugins and ! $manage_datadirs {
     warning('jenkins::purge_plugins has no effect unless jenkins::manage_datadirs is true')
@@ -394,17 +377,20 @@ class jenkins (
       notice(sprintf('INFO: make sure you install the following plugins with your code using this module: %s',join($jenkins::params::default_plugins,','))) # lint:ignore:140chars
     }
 
-    if $service_provider == 'systemd' {
-      jenkins::systemd { 'jenkins':
-        user   => $user,
-        libdir => $libdir,
-      }
+    # puppet/jenkins used to implement systemd but Jenkins 2.332 moved to
+    # systemd and implements this natively. Clean up the old implementation.
+    $old_libdir = $facts['os']['family'] ? {
+      'Archlinux' => '/usr/share/java/jenkins/',
+      'Debian'    => '/usr/share/jenkins',
+      default     => '/usr/lib/jenkins',
+    }
+    file { "${old_libdir}/jenkins-run":
+      ensure  => absent,
+    }
 
-      # jenkins::config manages the jenkins user resource, which is autorequired
-      # by the file resource for the run wrapper.
-      Class['jenkins::config']
-      -> Jenkins::Systemd['jenkins']
-      -> Anchor['jenkins::end']
+    file { '/etc/systemd/system/jenkins.service':
+      ensure => absent,
+      notify => Service['jenkins'],
     }
   }
 

commit e8aee23361a309fba29726a890b2113103b693fc

Move systemd overrides to user_setup for Debian
On Debian services are started automatically during package
installation. The setup wizard is configured via the systemd override so
creating this prior to package installation works around it. On
non-Debian (where it isn't autostarted) it doesn't matter and managing
all platforms the same has benefits.

The notify_service was missing which caused the process to run with an
incorrect parameter. This also adds a test to verify the argument is
really there when running. It should be noted that on Ubuntu 20.04 I
noticed a weird bug where it wasn't picked up. The test did catch it so
when the OS version support is added it'll be easier to catch.

Fixes: 98f46ca999635d0a9a2f4ad84d70ed60389f7b47

manifests/config.pp
@@ -4,14 +4,4 @@ class jenkins::config {
   assert_private()
 
   ensure_resource('jenkins::plugin', $jenkins::default_plugins)
-
-  $config_hash = merge(
-    $jenkins::params::config_hash_defaults,
-    $jenkins::config_hash
-  )
-
-  systemd::dropin_file { 'puppet-overrides.conf':
-    unit    => 'jenkins.service',
-    content => epp("${module_name}/jenkins-override.epp", { 'environment' => $config_hash }),
-  }
 }

manifests/user_setup.pp
@@ -52,4 +52,17 @@ class jenkins::user_setup {
     ensure_resource('file', $jenkins::plugin_dir, $plugin_dir_params)
     ensure_resource('file', $jenkins::job_dir, $dir_params)
   }
+
+  # On Debian the service is started by default so it must be configured prior
+  # to installation which is why it's configured in this file rather than config.pp
+  $config_hash = merge(
+    $jenkins::params::config_hash_defaults,
+    $jenkins::config_hash
+  )
+
+  systemd::dropin_file { 'puppet-overrides.conf':
+    unit           => 'jenkins.service',
+    content        => epp("${module_name}/jenkins-override.epp", { 'environment' => $config_hash }),
+    notify_service => true,
+  }
 }

spec/acceptance/class_spec.rb
@@ -36,6 +36,11 @@ describe 'jenkins class' do
       it { is_expected.to be_running }
       it { is_expected.to be_enabled }
     end
+
+    describe process('java') do
+      it { is_expected.to be_running }
+      its(:args) { is_expected.to match(%r{-Djenkins\.install\.runSetupWizard=false}) }
+    end
   end
 
   context 'executors' do