b89c4164bcabb9c745ee1caf32714fe5ea9ab8d0
Sally Young <github@justa.fish>
2024-06-11 12:38:09 +0100

    Fix indentation

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -5,9 +5,9 @@ class jenkins::firewall {
   assert_private()
 
   firewall { '500 allow Jenkins inbound traffic':
-    jump   => 'accept',
-    state  => 'NEW',
-    dport  => [jenkins_port()],
-    proto  => 'tcp',
+    jump  => 'accept',
+    state => 'NEW',
+    dport => [jenkins_port()],
+    proto => 'tcp',
   }
 }
