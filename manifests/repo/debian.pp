d51ce12da0c8235db45c0e568272ad4189e13dcc
Zen Fu <zen@systemli.org>
2025-08-17 07:56:22 -0300

    do not verify the APT key fingerprint
    Verifying the APT key fingerprint should really be a responsibility of
    the `puppetlabs-apt` module, so we deliberately choose to not do it
    here.
    
    Note that, currenlty, neither the (obsolete) `apt::key` nor the
    `apt::keyring` resources do key fingerprint verification, so this change
    isn't really a regression. Here's an upstream issue:
    
        https://github.com/puppetlabs/puppetlabs-apt/issues/1175
    
    Before this change, the key fingerprint was only used (by `apt::key`) to
    name the file where the key was stored. Because no verification is made
    either here or on the `puppetlabs-apt` side, we also remove the
    `$gpg_key_id` class param. This change avoids the need to maintain the
    hardcoded fingerprint, while also avoiding a false impression that they
    fingerprint is somehow verified.
    
    Discussion: https://github.com/voxpupuli/puppet-jenkins/pull/1130

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,8 +1,6 @@
 # @summary Set up the apt repo on Debian-based distros
 # @api private
-class jenkins::repo::debian (
-  String $gpg_key_id = '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
-) {
+class jenkins::repo::debian {
   assert_private()
 
   include apt
@@ -23,12 +21,5 @@ class jenkins::repo::debian (
       'name'   => 'jenkins.asc',
       'source' => "${location}/${jenkins::repo::gpg_key_filename}",
     },
-    notify   => Exec['check Jenkins OpenPGP key fingerprint'],
-  }
-
-  exec { 'check Jenkins OpenPGP key fingerprint':
-    command     => "/usr/bin/test \"$(/usr/bin/gpg --show-keys --with-colons /etc/apt/keyrings/jenkins.asc | /usr/bin/awk -F: '/^fpr/ {print \$10}' | head -n 1)\" = ${gpg_key_id}",
-    refreshonly => true,
-    require     => Apt::Source['jenkins'],
   }
 }
